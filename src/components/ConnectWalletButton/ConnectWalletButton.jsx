7fd64fa1afa9b447a2a04a41270b6c0c1486560f
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-07-25 14:14:25 +0300

    Connecting a wallet

--- src/components/ConnectWalletButton/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton/ConnectWalletButton.jsx
@@ -1,7 +1,58 @@
+import { ethers } from 'ethers';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './ConnectWalletButton.css';
 
 const ConnectWalletButton = () => {
-  return <button className="button">Connect wallet</button>;
+  // Функція для підключення гаманця
+  const connectWallet = async () => {
+    // Перевірка чи встановлено MetaMask
+    if (!window.ethereum) {
+      // Виведення повідомлення з посиланням на MetaMask
+      toast.error(
+        <>
+          Встановіть{' '}
+          <a
+            href="https://metamask.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MetaMask
+          </a>{' '}
+          у вашому браузері
+        </>,
+        {
+          closeButton: true, // Додає кнопку "закрити"
+        }
+      );
+      return;
+    }
+
+    try {
+      // Підключення гаманця
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      console.log('Підключено гаманець з адресою:', address);
+    } catch (error) {
+      console.error('Помилка підключення гаманця:', error);
+      // Виведення повідомлення про помилку з кнопкою "закрити"
+      toast.error('Помилка підключення гаманця', {
+        closeButton: true,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <button className="button" onClick={connectWallet}>
+        Connect wallet
+      </button>
+      <ToastContainer /> {/* Контейнер для виведення повідомлень */}
+    </div>
+  );
 };
 
 export default ConnectWalletButton;
