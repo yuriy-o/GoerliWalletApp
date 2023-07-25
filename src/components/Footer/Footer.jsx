149134e04bb9154dba54f41826563de0baedf64b
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-07-25 20:40:19 +0300

    Adds responsive styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <span>
+      <span className="span">
         Project by{' '}
         <a
           href="https://github.com/yuriy-o"
